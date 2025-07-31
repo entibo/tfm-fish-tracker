3a924296778df8798be4e4febb09a9c341cc3938
entibo <thibautguenedal@gmail.com>
2025-07-31 23:05:56 +0200

    Show coin value of relics

--- src/components/MapViewer.vue
+++ src/components/MapViewer.vue
@@ -6,84 +6,84 @@ import RelicIcon from './RelicIcon.vue'
 <template>
   <div class="detailed-maps">
     <DetailedMap map="waterfall">
-      <RelicIcon relic="F1" style="left: 640px; top: 0px"></RelicIcon>
-      <RelicIcon relic="F5" style="left: 680px; top: 0px"></RelicIcon>
-      <RelicIcon relic="D6" style="left: 660px; top: 270px"></RelicIcon>
-      <RelicIcon relic="E4" style="left: 700px; top: 270px"></RelicIcon>
-      <RelicIcon relic="E5" style="left: 180px; top: 130px"></RelicIcon>
-      <RelicIcon relic="E10" style="left: 220px; top: 130px"></RelicIcon>
-      <RelicIcon relic="B10" style="left: 40px; top: 300px"></RelicIcon>
-      <RelicIcon relic="F10" style="left: 80px; top: 300px"></RelicIcon>
+      <RelicIcon coins relic="F1" style="left: 640px; top: 0px"></RelicIcon>
+      <RelicIcon coins relic="F5" style="left: 680px; top: 0px"></RelicIcon>
+      <RelicIcon coins relic="D6" style="left: 660px; top: 270px"></RelicIcon>
+      <RelicIcon coins relic="E4" style="left: 700px; top: 270px"></RelicIcon>
+      <RelicIcon coins relic="E5" style="left: 180px; top: 130px"></RelicIcon>
+      <RelicIcon coins relic="E10" style="left: 220px; top: 130px"></RelicIcon>
+      <RelicIcon coins relic="B10" style="left: 40px; top: 300px"></RelicIcon>
+      <RelicIcon coins relic="F10" style="left: 80px; top: 300px"></RelicIcon>
     </DetailedMap>
     <DetailedMap map="jungle">
-      <RelicIcon relic="C4" style="left: 85px; top: 50px"></RelicIcon>
-      <RelicIcon relic="F7" style="left: 125px; top: 50px"></RelicIcon>
-      <RelicIcon relic="B1" style="left: 365px; top: 160px"></RelicIcon>
-      <RelicIcon relic="D10" style="left: 405px; top: 160px"></RelicIcon>
-      <RelicIcon relic="C3" style="left: 670px; top: 0px"></RelicIcon>
-      <RelicIcon relic="D8" style="left: 710px; top: 0px"></RelicIcon>
-      <RelicIcon relic="C9" style="left: 85px; top: 260px"></RelicIcon>
-      <RelicIcon relic="D7" style="left: 125px; top: 260px"></RelicIcon>
+      <RelicIcon coins relic="C4" style="left: 85px; top: 50px"></RelicIcon>
+      <RelicIcon coins relic="F7" style="left: 125px; top: 50px"></RelicIcon>
+      <RelicIcon coins relic="B1" style="left: 365px; top: 160px"></RelicIcon>
+      <RelicIcon coins relic="D10" style="left: 405px; top: 160px"></RelicIcon>
+      <RelicIcon coins relic="C3" style="left: 670px; top: 0px"></RelicIcon>
+      <RelicIcon coins relic="D8" style="left: 710px; top: 0px"></RelicIcon>
+      <RelicIcon coins relic="C9" style="left: 85px; top: 260px"></RelicIcon>
+      <RelicIcon coins relic="D7" style="left: 125px; top: 260px"></RelicIcon>
     </DetailedMap>
     <DetailedMap map="island">
-      <RelicIcon relic="B7" style="left: 685px; top: 50px"></RelicIcon>
-      <RelicIcon relic="A1" style="left: 410px; top: 110px"></RelicIcon>
-      <RelicIcon relic="B3" style="left: 450px; top: 110px"></RelicIcon>
-      <RelicIcon relic="E3" style="left: 490px; top: 110px"></RelicIcon>
-      <RelicIcon relic="F9" style="left: 100px; top: 300px"></RelicIcon>
-      <RelicIcon relic="A10" style="left: 700px; top: 300px"></RelicIcon>
-      <RelicIcon relic="F8" style="left: 740px; top: 300px"></RelicIcon>
+      <RelicIcon coins relic="B7" style="left: 685px; top: 50px"></RelicIcon>
+      <RelicIcon coins relic="A1" style="left: 410px; top: 110px"></RelicIcon>
+      <RelicIcon coins relic="B3" style="left: 450px; top: 110px"></RelicIcon>
+      <RelicIcon coins relic="E3" style="left: 490px; top: 110px"></RelicIcon>
+      <RelicIcon coins relic="F9" style="left: 100px; top: 300px"></RelicIcon>
+      <RelicIcon coins relic="A10" style="left: 700px; top: 300px"></RelicIcon>
+      <RelicIcon coins relic="F8" style="left: 740px; top: 300px"></RelicIcon>
     </DetailedMap>
     <DetailedMap map="boat">
-      <RelicIcon relic="A7" style="left: 480px; top: 220px"></RelicIcon>
-      <RelicIcon relic="B3" style="left: 520px; top: 220px"></RelicIcon>
+      <RelicIcon coins relic="A7" style="left: 480px; top: 220px"></RelicIcon>
+      <RelicIcon coins relic="B3" style="left: 520px; top: 220px"></RelicIcon>
     </DetailedMap>
     <DetailedMap map="dinosaur">
-      <RelicIcon relic="E8" style="left: 660px; top: 40px"></RelicIcon>
-      <RelicIcon relic="B2" style="left: 700px; top: 40px"></RelicIcon>
-      <RelicIcon relic="C1" style="left: 630px; top: 320px"></RelicIcon>
-      <RelicIcon relic="D1" style="left: 670px; top: 320px"></RelicIcon>
-      <RelicIcon relic="E7" style="left: 710px; top: 320px"></RelicIcon>
-      <RelicIcon relic="C10" style="left: 195px; top: 0px"></RelicIcon>
-      <RelicIcon relic="E2" style="left: 235px; top: 0px"></RelicIcon>
-      <RelicIcon relic="C1" style="left: 380px; top: 320px"></RelicIcon>
-      <RelicIcon relic="F4" style="left: 420px; top: 320px"></RelicIcon>
+      <RelicIcon coins relic="E8" style="left: 660px; top: 40px"></RelicIcon>
+      <RelicIcon coins relic="B2" style="left: 700px; top: 40px"></RelicIcon>
+      <RelicIcon coins relic="C1" style="left: 630px; top: 320px"></RelicIcon>
+      <RelicIcon coins relic="D1" style="left: 670px; top: 320px"></RelicIcon>
+      <RelicIcon coins relic="E7" style="left: 710px; top: 320px"></RelicIcon>
+      <RelicIcon coins relic="C10" style="left: 195px; top: 0px"></RelicIcon>
+      <RelicIcon coins relic="E2" style="left: 235px; top: 0px"></RelicIcon>
+      <RelicIcon coins relic="C1" style="left: 380px; top: 320px"></RelicIcon>
+      <RelicIcon coins relic="F4" style="left: 420px; top: 320px"></RelicIcon>
     </DetailedMap>
     <DetailedMap map="shipwreck">
-      <RelicIcon relic="A2" style="left: 500px; top: 70px"></RelicIcon>
-      <RelicIcon relic="C6" style="left: 540px; top: 70px"></RelicIcon>
-      <RelicIcon relic="A3" style="left: 720px; top: 160px"></RelicIcon>
-      <RelicIcon relic="A8" style="left: 760px; top: 160px"></RelicIcon>
-      <RelicIcon relic="B6" style="left: 20px; top: 310px"></RelicIcon>
-      <RelicIcon relic="F3" style="left: 60px; top: 310px"></RelicIcon>
-      <RelicIcon relic="D9" style="left: 650px; top: 280px"></RelicIcon>
-      <RelicIcon relic="F2" style="left: 690px; top: 280px"></RelicIcon>
-      <RelicIcon relic="F9" style="left: 730px; top: 280px"></RelicIcon>
+      <RelicIcon coins relic="A2" style="left: 500px; top: 70px"></RelicIcon>
+      <RelicIcon coins relic="C6" style="left: 540px; top: 70px"></RelicIcon>
+      <RelicIcon coins relic="A3" style="left: 720px; top: 160px"></RelicIcon>
+      <RelicIcon coins relic="A8" style="left: 760px; top: 160px"></RelicIcon>
+      <RelicIcon coins relic="B6" style="left: 20px; top: 310px"></RelicIcon>
+      <RelicIcon coins relic="F3" style="left: 60px; top: 310px"></RelicIcon>
+      <RelicIcon coins relic="D9" style="left: 650px; top: 280px"></RelicIcon>
+      <RelicIcon coins relic="F2" style="left: 690px; top: 280px"></RelicIcon>
+      <RelicIcon coins relic="F9" style="left: 730px; top: 280px"></RelicIcon>
     </DetailedMap>
     <DetailedMap map="antic">
-      <RelicIcon relic="D2" style="left: 220px; top: 40px"></RelicIcon>
-      <RelicIcon relic="A6" style="left: 340px; top: 40px"></RelicIcon>
-      <RelicIcon relic="C2" style="left: 380px; top: 40px"></RelicIcon>
-      <RelicIcon relic="D4" style="left: 590px; top: 40px"></RelicIcon>
-      <RelicIcon relic="A5" style="left: 630px; top: 40px"></RelicIcon>
-      <RelicIcon relic="B4" style="left: 750px; top: 40px"></RelicIcon>
-      <RelicIcon relic="B5" style="left: 740px; top: 330px"></RelicIcon>
-      <RelicIcon relic="D5" style="left: 530px; top: 280px"></RelicIcon>
-      <RelicIcon relic="A4" style="left: 260px; top: 280px"></RelicIcon>
-      <RelicIcon relic="C8" style="left: 50px; top: 330px"></RelicIcon>
+      <RelicIcon coins relic="D2" style="left: 220px; top: 40px"></RelicIcon>
+      <RelicIcon coins relic="A6" style="left: 340px; top: 40px"></RelicIcon>
+      <RelicIcon coins relic="C2" style="left: 380px; top: 40px"></RelicIcon>
+      <RelicIcon coins relic="D4" style="left: 590px; top: 40px"></RelicIcon>
+      <RelicIcon coins relic="A5" style="left: 630px; top: 40px"></RelicIcon>
+      <RelicIcon coins relic="B4" style="left: 750px; top: 40px"></RelicIcon>
+      <RelicIcon coins relic="B5" style="left: 740px; top: 330px"></RelicIcon>
+      <RelicIcon coins relic="D5" style="left: 530px; top: 280px"></RelicIcon>
+      <RelicIcon coins relic="A4" style="left: 260px; top: 280px"></RelicIcon>
+      <RelicIcon coins relic="C8" style="left: 50px; top: 330px"></RelicIcon>
     </DetailedMap>
     <DetailedMap map="crystal">
-      <RelicIcon relic="C7" style="left: 700px; top: 100px"></RelicIcon>
-      <RelicIcon relic="D3" style="left: 560px; top: 100px"></RelicIcon>
-      <RelicIcon relic="E1" style="left: 170px; top: 20px"></RelicIcon>
-      <RelicIcon relic="B8" style="left: 210px; top: 20px"></RelicIcon>
-      <RelicIcon relic="F6" style="left: 460px; top: 220px"></RelicIcon>
-      <RelicIcon relic="B9" style="left: 500px; top: 220px"></RelicIcon>
-      <RelicIcon relic="D1" style="left: 290px; top: 220px"></RelicIcon>
-      <RelicIcon relic="C5" style="left: 330px; top: 220px"></RelicIcon>
-      <RelicIcon relic="A9" style="left: 480px; top: 310px"></RelicIcon>
-      <RelicIcon relic="E9" style="left: 280px; top: 310px"></RelicIcon>
-      <RelicIcon relic="E6" style="left: 320px; top: 310px"></RelicIcon>
+      <RelicIcon coins relic="C7" style="left: 700px; top: 100px"></RelicIcon>
+      <RelicIcon coins relic="D3" style="left: 560px; top: 100px"></RelicIcon>
+      <RelicIcon coins relic="E1" style="left: 170px; top: 20px"></RelicIcon>
+      <RelicIcon coins relic="B8" style="left: 210px; top: 20px"></RelicIcon>
+      <RelicIcon coins relic="F6" style="left: 460px; top: 220px"></RelicIcon>
+      <RelicIcon coins relic="B9" style="left: 500px; top: 220px"></RelicIcon>
+      <RelicIcon coins relic="D1" style="left: 290px; top: 220px"></RelicIcon>
+      <RelicIcon coins relic="C5" style="left: 330px; top: 220px"></RelicIcon>
+      <RelicIcon coins relic="A9" style="left: 480px; top: 310px"></RelicIcon>
+      <RelicIcon coins relic="E9" style="left: 280px; top: 310px"></RelicIcon>
+      <RelicIcon coins relic="E6" style="left: 320px; top: 310px"></RelicIcon>
     </DetailedMap>
   </div>
 </template>
--- src/components/RelicIcon.vue
+++ src/components/RelicIcon.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { relicStore } from '@/stores/relics.ts'
+import relicValueMap from '@/relics-value.json'
 
 const { relic } = defineProps<{
   relic: string
+  coins: boolean
 }>()
 
 const imageUrl = `/img/relic/${relic}.jpg`
+const numCoins = relicValueMap[relic]
 </script>
 
 <template>
@@ -21,6 +24,11 @@ const imageUrl = `/img/relic/${relic}.jpg`
   >
     <img :src="imageUrl" width="40" height="40" />
     <div class="relic-name">{{ relic }}</div>
+    <div class="coins" v-if="coins">
+      <div v-for="n in numCoins" :key="n" class="coin">
+        <img src="/img/smol-coin.png" width="15" height="17" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,8 +49,8 @@ const imageUrl = `/img/relic/${relic}.jpg`
     -1px -1px 1px black,
     1px 1px 1px black,
     0px 0px 5px black;
-  bottom: 2px;
-  right: 3px;
+  top: 2px;
+  left: 3px;
   pointer-events: none;
 }
 .checked .relic-name {
@@ -53,6 +61,18 @@ const imageUrl = `/img/relic/${relic}.jpg`
   opacity: 60%;
 }
 
+.coins {
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  left: 0;
+  bottom: -2px;
+  height: 16px;
+  right: 1px;
+}
+.coin {
+  width: 11px;
+}
 img {
   cursor: pointer;
 }
