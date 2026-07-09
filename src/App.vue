c75c21ae77831b8ae3f7c46fcec0d0109bc8ac18
Thibaut Guénédal <thibautguenedal@gmail.com>
2026-07-09 21:07:46 +0200

    2026

--- src/App.vue
+++ src/App.vue
@@ -21,16 +21,8 @@ import OpenLinkIcon from './components/OpenLinkIcon.vue'
   <MapBrowser></MapBrowser>
   <div class="credits text-block">
     <div>
-      Thanks to everyone who contributed information on Discord &
-      <a
-        href="https://docs.google.com/spreadsheets/d/193P_yJbU66j8pkPUudUnoXi1M708POSIbds2UuXUiIo/"
-        target="_blank"
-        >the Google Sheet</a
-      >
-      &
-      <a href="https://breezewiki.com/transformice/wiki/Fishing_2026" target="_blank"
-        >Transformice Wiki</a
-      >
+      <span>🧀</span> More information on
+      <a href="https://transformice.fandom.com/wiki/Fishing_2026" target="_blank">the Wiki</a>
     </div>
     <div class="source-code">
       <a href="https://github.com/entibo/tfm-fish-tracker" target="_blank">Source code</a>
