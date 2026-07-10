ec35aa7e9b235dd093b60180bb7f141309a5d4ff
Thibaut Guénédal <thibautguenedal@gmail.com>
2026-07-10 21:32:39 +0200

    Adjust placement of relics

--- src/data.ts
+++ src/data.ts
@@ -29,14 +29,14 @@ export const fishingSpotCoordinates: Record<
     y: number
   }
 > = {
-  waterfall1: { x: 680, y: 20 },
+  waterfall1: { x: 680, y: 30 },
   waterfall2: { x: 700, y: 290 },
-  waterfall3: { x: 220, y: 150 },
-  waterfall4: { x: 80, y: 320 },
+  waterfall3: { x: 220, y: 160 },
+  waterfall4: { x: 80, y: 330 },
 
   jungle1: { x: 125, y: 70 },
   jungle2: { x: 405, y: 180 },
-  jungle3: { x: 710, y: 20 },
+  jungle3: { x: 710, y: 30 },
   jungle4: { x: 125, y: 280 },
 
   island1: { x: 705, y: 70 },
@@ -44,7 +44,7 @@ export const fishingSpotCoordinates: Record<
   island3: { x: 120, y: 320 },
   island4: { x: 740, y: 320 },
 
-  boat1: { x: 520, y: 240 },
+  boat1: { x: 520, y: 210 },
 
   dinosaur1: { x: 700, y: 60 },
   dinosaur2: { x: 690, y: 340 },
@@ -52,16 +52,16 @@ export const fishingSpotCoordinates: Record<
   dinosaur4: { x: 420, y: 340 },
 
   shipwreck1: { x: 540, y: 90 },
-  shipwreck2: { x: 760, y: 180 },
+  shipwreck2: { x: 740, y: 180 },
   shipwreck3: { x: 60, y: 330 },
   shipwreck4: { x: 710, y: 300 },
 
   antic1: { x: 240, y: 60 },
   antic2: { x: 380, y: 60 },
-  antic3: { x: 630, y: 60 },
-  antic4: { x: 750, y: 60 },
+  antic3: { x: 620, y: 60 },
+  antic4: { x: 740, y: 60 },
   antic5: { x: 70, y: 350 },
-  antic6: { x: 760, y: 350 },
+  antic6: { x: 740, y: 350 },
   antic7: { x: 280, y: 300 },
   antic8: { x: 550, y: 300 },
 
